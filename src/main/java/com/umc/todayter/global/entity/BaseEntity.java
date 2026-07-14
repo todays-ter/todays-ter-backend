158a1f698fc2cc43879a49f424e7e7aa26268923
HAN JONGSEO <pantene74@kw.ac.kr>
2026-07-14 19:35:46 +0900

    Chore: BaseEntity 추가 (#6)

---
+++ src/main/java/com/umc/todayter/global/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.umc.todayter.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass // 이 클래스는 테이블로 생성되지 않음
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public abstract class BaseEntity {
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at") // nullable
+    private LocalDateTime updatedAt;
+}
