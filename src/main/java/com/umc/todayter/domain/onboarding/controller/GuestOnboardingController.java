7f9210f9a2135bbd5df6d709b184e73b8d7228a3
HAN JONGSEO <pantene74@kw.ac.kr>
2026-07-19 19:47:29 +0900

    Feat: 고민 유형 저장/수정 API 구현 (#8)

--- src/main/java/com/umc/todayter/domain/onboarding/controller/GuestOnboardingController.java
+++ src/main/java/com/umc/todayter/domain/onboarding/controller/GuestOnboardingController.java
@@ -1,6 +1,8 @@
 package com.umc.todayter.domain.onboarding.controller;
 
+import com.umc.todayter.domain.onboarding.dto.request.GuestConcernRequest;
 import com.umc.todayter.domain.onboarding.dto.request.GuestSajuRequest;
+import com.umc.todayter.domain.onboarding.dto.response.GuestConcernResponse;
 import com.umc.todayter.domain.onboarding.dto.response.GuestSajuResponse;
 import com.umc.todayter.domain.onboarding.enums.code.OnboardingSuccessCode;
 import com.umc.todayter.domain.onboarding.service.GuestOnboardingService;
@@ -32,5 +34,20 @@ public class GuestOnboardingController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiResponse.onSuccess(result, OnboardingSuccessCode.SAJU_SAVED));
+    }
+
+    @PutMapping("/concerns")
+    public ResponseEntity<ApiResponse<GuestConcernResponse>> saveConcerns(
+            @CookieValue(
+                    name = GuestCookieUtil.COOKIE_NAME,
+                    required = false
+            ) String guestId,
+            @Valid @RequestBody GuestConcernRequest request
+    ) {
+        GuestConcernResponse result = guestOnboardingService.saveConcerns(guestId, request);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.onSuccess(result, OnboardingSuccessCode.CONCERNS_SAVED));
     }
 }
